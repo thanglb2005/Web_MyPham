044bed01133e5a8851047af4fb0f4a27fdcec783
Nguyneee <nguyendh6g@Gmail.com>
2025-10-26 18:49:03 +0700

    push

--- OneShop/src/main/java/vn/controller/shipper/ShipperHomeController.java
+++ OneShop/src/main/java/vn/controller/shipper/ShipperHomeController.java
@@ -111,9 +111,10 @@ public class ShipperHomeController {
             }
         }
 
-        // Lấy tất cả đơn hàng được gán cho shipper này (bao gồm cả CONFIRMED)
-        List<Order> allAssignedOrders = orderRepository.findOrdersByShipper(shipper);
+        // Lấy các đơn hàng được phân công cho shipper này
+        List<Order> assignedOrders = orderRepository.findOrdersByShipper(shipper);
         
+<<<<<<< HEAD
         // Debug: Log tất cả đơn hàng được gán
         System.out.println("All assigned orders for shipper: " + allAssignedOrders.size());
         for (Order order : allAssignedOrders) {
@@ -163,6 +164,14 @@ public class ShipperHomeController {
         for (Order order : assignedOrders) {
             System.out.println("Assigned order #" + order.getOrderId() + " - Status: " + order.getStatus());
         }
+=======
+        // Lấy các đơn hàng đang chờ giao (CONFIRMED) mà chưa có shipper
+        // Chỉ lấy đơn hàng từ các shop mà shipper được gán
+        List<Order> availableOrders = orderRepository.findAvailableOrdersForShipper(
+            shipper,
+            Order.OrderStatus.CONFIRMED
+        );
+>>>>>>> parent of 7dd478a (đơn hỏa tóc và đơn giao xa)
 
         // Lấy các đơn hàng giao muộn của shipper
         List<Order> overdueOrders = orderService.findOverdueOrdersByShipper(shipper);
@@ -207,11 +216,10 @@ public class ShipperHomeController {
         }
 
         try {
-            // Kiểm tra đơn hàng có tồn tại và có thể nhận
+            // Kiểm tra đơn hàng có tồn tại và chưa được phân công shipper
             Order order = orderService.getOrderById(orderId);
-            if (order != null && 
-                (order.getStatus() == Order.OrderStatus.CONFIRMED || order.getStatus() == Order.OrderStatus.SHIPPING) &&
-                (order.getShipper() == null || order.getShipper().getUserId().equals(shipper.getUserId()))) {
+            if (order != null && order.getShipper() == null && 
+                order.getStatus() == Order.OrderStatus.CONFIRMED) {
                 
                 // Phân công shipper cho đơn hàng
                 orderService.assignShipper(orderId, shipper);
